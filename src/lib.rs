1317f71975769da005715a8b42d3879379d37ab5
Sigurd <sigurd-s@online.no>
2023-06-25 01:32:24 +0200

    Add trait requirements for Array

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(generic_const_exprs)]
 #![feature(iter_array_chunks)]
 
+use std::{borrow::{Borrow, BorrowMut}, hash::Hash, slice::{Iter, IterMut}, ops::{Index, Range, RangeInclusive, RangeFrom, RangeTo, RangeToInclusive, IndexMut, RangeFull, Deref}};
+
 mod private
 {
     pub trait Array {}
@@ -8,8 +10,31 @@ mod private
 }
 
 pub trait Array: private::Array
+    + Sized
+    + IntoIterator//<IntoIter = std::array::IntoIter<<Self as IntoIterator>::Item, {Self::_LENGTH}>>
+    + AsRef<[Self::Item]>
+    + AsMut<[Self::Item]>
+    + Borrow<[Self::Item]>
+    + BorrowMut<[Self::Item]>
+    + Index<usize, Output = <[Self::Item] as Index<usize>>::Output>
+    + Index<Range<usize>, Output = <[Self::Item] as Index<Range<usize>>>::Output>
+    + Index<RangeInclusive<usize>, Output = <[Self::Item] as Index<RangeInclusive<usize>>>::Output>
+    + Index<RangeFrom<usize>, Output = <[Self::Item] as Index<RangeFrom<usize>>>::Output>
+    + Index<RangeTo<usize>, Output = <[Self::Item] as Index<RangeTo<usize>>>::Output>
+    + Index<RangeToInclusive<usize>, Output = <[Self::Item] as Index<RangeToInclusive<usize>>>::Output>
+    + Index<RangeFull, Output = <[Self::Item] as Index<RangeFull>>::Output>
+    + IndexMut<usize>
+    + IndexMut<Range<usize>>
+    + IndexMut<RangeInclusive<usize>>
+    + IndexMut<RangeFrom<usize>>
+    + IndexMut<RangeTo<usize>>
+    + IndexMut<RangeToInclusive<usize>>
+    + IndexMut<RangeFull>
+where
+    for<'a> &'a Self: TryFrom<&'a [Self::Item]>
+        + IntoIterator<Item = &'a Self::Item, IntoIter = Iter<'a, Self::Item>>,
+    for<'a> &'a mut Self: TryFrom<&'a mut [Self::Item]> + IntoIterator<Item = &'a mut Self::Item, IntoIter = IterMut<'a, Self::Item>>
 {
-    type Item;
     const LENGTH: usize;
 
     fn into_array(self) -> [Self::Item; Self::LENGTH];
